e2b6eac26e2de21e6e5a177958a835c9d99797b0
CatBoxy <thecatbox@protonmail.com>
2024-07-16 15:52:36 -0300

    telegram verification

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { bot } from "./bot";
-import { sendAlertRouter } from "./routes";
+import { routes } from "./routes";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api/send-alert", sendAlertRouter);
+app.use("/api", routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { Request, Response } from "express";
 import { sendTelegramAlert } from "./bot";
+import { createClient } from "@supabase/supabase-js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/send-alert", async (req, res) => {
   const { userId, message } = req.body;
 
   if (!userId || !message) {
@@ -19,4 +21,42 @@ router.post("/", async (req, res) => {
   }
 });
 
-export const sendAlertRouter = router;
+router.post("/link-telegram", async (req: Request, res: Response) => {
+  const { token, userId } = req.body;
+  const serviceKey: string = process.env.SERVICE_KEY!;
+  const supabaseUrl: string = process.env.SUPABASE_URL!;
+  const telegramTokenTable: string = "Telegram_token";
+  const userTable: string = "User";
+  const supabase = createClient(supabaseUrl, serviceKey);
+
+  const { data: tokenData, error: tokenError } = await supabase
+    .from(telegramTokenTable)
+    .select("telegram_user_id")
+    .eq("token", token)
+    .single();
+
+  if (tokenError || !tokenData) {
+    return res.status(400).send({ message: "Token vencida o no existente." });
+  }
+
+  const telegramId = tokenData.telegram_user_id;
+
+  const { data, error } = await supabase
+    .from(userTable)
+    .update({ telegram_id: telegramId })
+    .eq("uuid", userId);
+
+  if (error) {
+    return res
+      .status(500)
+      .send({ message: "Error al enlazar cuenta de Telegram." });
+  }
+
+  await supabase.from(telegramTokenTable).delete().eq("token", token);
+
+  res
+    .status(200)
+    .send({ message: "Cuenta de Telegram enlazada satisfactoriamente." });
+});
+
+export const routes = router;
